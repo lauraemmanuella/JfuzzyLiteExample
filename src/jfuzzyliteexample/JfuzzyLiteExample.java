6f04bb56160122e71c399a5091dfe796c9c81ea9
Laura <Laura@10.77.22.86>
2018-09-11 19:32:21 -0300

    Atualizaçao do problema para Tipper Example

--- src/jfuzzyliteexample/JfuzzyLiteExample.java
+++ src/jfuzzyliteexample/JfuzzyLiteExample.java
@@ -4,10 +4,11 @@ import com.fuzzylite.*;
 import com.fuzzylite.activation.General;
 import com.fuzzylite.defuzzifier.Centroid;
 import com.fuzzylite.norm.s.Maximum;
-import com.fuzzylite.norm.t.AlgebraicProduct;
+import com.fuzzylite.norm.t.Minimum;
 import com.fuzzylite.rule.Rule;
 import com.fuzzylite.rule.RuleBlock;
-import com.fuzzylite.term.Ramp;
+import com.fuzzylite.term.Trapezoid;
+import com.fuzzylite.term.Triangle;
 import com.fuzzylite.variable.InputVariable;
 import com.fuzzylite.variable.OutputVariable;
 
@@ -15,63 +16,56 @@ public class JfuzzyLiteExample {
 
     public static void main(String[] args){
         Engine engine = new Engine();
-        engine.setName("ObstacleAvoidance");
-        engine.setDescription("");
+        engine.setName("Tipper");
 
-        InputVariable obstacle = new InputVariable();
-        obstacle.setName("obstacle");
-        obstacle.setDescription("");
-        obstacle.setEnabled(true);
-        obstacle.setRange(0.000, 1.000);
-        obstacle.setLockValueInRange(false);
-        //ao inves de Ramp poderia ser:
-        //new Triangle("D", 0.500, 1.000, 1.500)
-        //new Trapezoid("E", 1.000, 1.250, 1.750, 2.000)
-        obstacle.addTerm(new Ramp("left", 1.000, 0.000)); 
-        obstacle.addTerm(new Ramp("right", 0.000, 1.000));
-        engine.addInputVariable(obstacle);
+        InputVariable service = new InputVariable();
+        service.setName("service");
+        service.setRange(0.0, 10.0);
+       
+        service.addTerm(new Triangle("poor", -1, 0, 4)); 
+        service.addTerm(new Trapezoid("good", 1, 4, 6, 9));
+        service.addTerm(new Triangle("excelent", 6, 10, 11));
+        engine.addInputVariable(service);
+                
+        InputVariable food = new InputVariable();
+        food.setName("food");
+        food.setRange(0.0, 10.0);
+       
+        food.addTerm(new Trapezoid("rancid", -1, 0, 1, 3)); 
+        food.addTerm(new Triangle("delicious", 7, 10, 11));
+        engine.addInputVariable(food);
 
-        OutputVariable mSteer = new OutputVariable();
-        mSteer.setName("mSteer");
-        mSteer.setDescription("");
-        mSteer.setEnabled(true);
-        mSteer.setRange(0.000, 1.000);
-        mSteer.setLockValueInRange(false);
-        mSteer.setAggregation(new Maximum());
-        mSteer.setDefuzzifier(new Centroid(100));
-        mSteer.setDefaultValue(Double.NaN);
-        mSteer.setLockPreviousValue(false);
-        mSteer.addTerm(new Ramp("left", 1.000, 0.000));
-        mSteer.addTerm(new Ramp("right", 0.000, 1.000));
-        engine.addOutputVariable(mSteer);
+        OutputVariable tip = new OutputVariable();
+        tip.setName("tip");
+        tip.setRange(0.0, 30.0);
+        
+        tip.setAggregation(new Maximum());
+        tip.setDefuzzifier(new Centroid(100));
+        tip.setDefaultValue(0);
+        
+        tip.addTerm(new Triangle("cheap", 0, 5, 10));
+        tip.addTerm(new Triangle("average", 10, 15, 20));
+        tip.addTerm(new Triangle("generous", 20, 25, 30));
+        engine.addOutputVariable(tip);
 
         RuleBlock mamdani = new RuleBlock();
         mamdani.setName("mamdani");
-        mamdani.setDescription("");
-        mamdani.setEnabled(true);
-        mamdani.setConjunction(null);
-        mamdani.setDisjunction(null);
-        mamdani.setImplication(new AlgebraicProduct());
+        mamdani.setConjunction(new Minimum());
+        mamdani.setDisjunction(new Maximum());
+        mamdani.setImplication(new Minimum());
         mamdani.setActivation(new General());
-        mamdani.addRule(Rule.parse("if obstacle is left then mSteer is right", engine));
-        mamdani.addRule(Rule.parse("if obstacle is right then mSteer is left", engine));
+        mamdani.addRule(Rule.parse("if service is poor or food is rancid then tip is cheap", engine));
+        mamdani.addRule(Rule.parse("if service is good then tip is average", engine));
+        mamdani.addRule(Rule.parse("if service is excelent and food is delicious then tip is generous", engine));
         engine.addRuleBlock(mamdani);
 
+        InputVariable serviceinput = engine.getInputVariable("service");
+        InputVariable foodinput = engine.getInputVariable("food");
+        OutputVariable tipoutput = engine.getOutputVariable("tip");
 
-        StringBuilder status = new StringBuilder();
-        if (! engine.isReady(status))
-            throw new RuntimeException("[engine error] engine is not ready:n" + status);
-
-        InputVariable obstacle2 = engine.getInputVariable("obstacle");
-        OutputVariable steer = engine.getOutputVariable("mSteer");
-
-        for (int i = 0; i <= 50; ++i){
-            double location = obstacle2.getMinimum() + i * (obstacle2.range() / 50);
-            obstacle2.setValue(location);
-            engine.process();
-            FuzzyLite.logger().info(String.format(
-                    "obstacle.input = %s -> steer.output = %s",
-                    Op.str(location), Op.str(steer.getValue())));
-        }
+        serviceinput.setValue(10);
+        foodinput.setValue(10);
+        engine.process();
+        System.out.println("Service: " + serviceinput.getValue() + "\nFood:  " + foodinput.getValue() + "\nTip: " + tipoutput.getValue());
     }
 }
